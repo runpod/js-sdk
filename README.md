6cd77ddba73160593af357d22a1e27541983bb09
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2023-10-23 09:31:12 -0400

    migrate from node-sdk

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# js-sdk
-JS sdk for runpod
+# node-sdk
+
+Node JS client sdk for runpod
+
+# Example Usage
+
+```js
+const { RUNPOD_API_KEY, ENDPOINT_ID } = process.env
+import runpodSdk from "runpod/node-sdk"
+
+const runpod = runpodSdk(RUNPOD_API_KEY)
+const endpoint = runpod.endpoint.fromId(ENDPOINT_ID)
+const result = await endpoint.runRequest({
+  input: {
+    prompt: "a photo of a horse the size of a Boeing 787",
+  },
+})
+```
+
+# Using Endpoints
+
+Once an endpoint has been created, you can send requests to the queue:
+
+```js
+const requestId = await endpoint.queueRequest({
+  input: {
+    prompt: "a photo of a horse the size of a Boeing 787",
+  },
+})
+```
+
+You can check on the status of this request once you have the id:
+
+```js
+const status = await endpoint.getRequestStatus(requestId)
+```
+
+If the request has been completed, the status object returned will contain the `output` of the request.
+
+If you don't want to manage polling for request completion yourself, you can simply call `runRequest`, which will enqueue the request and then poll in a loop (by default, once every 10 seconds) until the request completes, fails or times out.
+
+```js
+const result = await endpoint.runRequest({
+  input: {
+    prompt: "a photo of a horse the size of a Boeing 787",
+  },
+})
+```
+
+If you have the id of a request, you can cancel it if it's taking too long or no longer necessary:
+
+```js
+await endpoint.cancelRequest(requestId)
+```
+
+For long running applications or troubleshooting, you may want to check the health of the endpoint workers:
+
+```js
+const health = await endpoint.getHealth()
+```
---
+++ serverless.ts
@@ -0,0 +1,142 @@
+import axios from "axios"
+import { curry } from "ramda"
+
+type ExecutionPolicy = {
+  executionTimeout: number
+}
+type S3Config = {
+  accessId: string
+  accessSecret: string
+  bucketName: string
+  endpointUrl: string
+}
+type EndpointInputPayload = {
+  input: any
+  webhook?: string
+  s3Config?: S3Config
+  policy?: ExecutionPolicy
+}
+type EndpointOutput = {
+  status: string
+  output: any
+}
+
+const runpodServerlessBaseUrl = "https://api.runpod.ai/v2"
+const getAuthHeader = (api_key: string) => ({
+  headers: {
+    "Authorization": `Bearer ${api_key}`,
+    "content-type": "application/json",
+  },
+})
+const maxWaitTimeSeconds = 300
+const print = console.log
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const handleErrors = async (axiosRequest) => {
+  const resp = await axiosRequest
+  const { status, statusText } = resp
+  if (status !== 200) {
+    return { status, statusText, started: false }
+  }
+  return { ...resp.data, started: true, completed: true, succeeded: true }
+}
+
+const getEndpointUrl = (endpointId) => `${runpodServerlessBaseUrl}/${endpointId}`
+
+//run and then poll status
+const runsync = curry(async (api_key, endpointId, request) => {
+  const runResp = await run(api_key, endpointId, request)
+  let data = runResp.data
+  const { id } = data
+  const authHeader = getAuthHeader(api_key)
+  const statusUrl = getEndpointUrl(endpointId) + "/status/" + id
+  const pollIntervalSeconds = 10
+  const start = Date.now()
+  while (!["COMPLETED", "FAILED"].includes(data.status)) {
+    if (Date.now() - start > maxWaitTimeSeconds * 1000) {
+      print(`${statusUrl} timed out after ${maxWaitTimeSeconds} seconds`)
+      return { ...data, started: true, completed: false }
+    }
+    await sleep(1000 * pollIntervalSeconds)
+    const statusResp = await axios.get(statusUrl, authHeader)
+    data = statusResp.data
+    print(`${statusUrl}: ${data.status}`)
+  }
+  return { output: data, started: true, completed: true, succeeded: data.status === "COMPLETED" }
+})
+
+//wrapper over /run
+const run = curry((api_key: string, endpointId: string, request) => {
+  const url = getEndpointUrl(endpointId) + "/run"
+  const authHeader = getAuthHeader(api_key)
+  return handleErrors(axios.post(url, request, authHeader))
+})
+//wrapper over /status
+const getStatus = curry((api_key: string, endpointId: string, requestId) => {
+  const url = getEndpointUrl(endpointId) + "/status/" + requestId
+  const authHeader = getAuthHeader(api_key)
+  return handleErrors(axios.get(url, authHeader))
+})
+//wrapper over /cancel
+const cancel = curry((api_key: string, endpointId: string, requestId) => {
+  const url = getEndpointUrl(endpointId) + "/cancel/" + requestId
+  const authHeader = getAuthHeader(api_key)
+  return handleErrors(axios.post(url, {}, authHeader))
+})
+//wrapper over /health
+const getHealth = curry((api_key: string, endpointId: string) => {
+  const url = getEndpointUrl(endpointId) + "/health"
+  const authHeader = getAuthHeader(api_key)
+  return handleErrors(axios.get(url, authHeader))
+})
+
+class Endpoint {
+  endpointId: string = ""
+  private api_key: string = ""
+  constructor(api_key: string, endpointId: string) {
+    this.api_key = api_key
+    this.endpointId = endpointId
+  }
+  async runsync(request: EndpointInputPayload): Promise<EndpointOutput> {
+    return runsync(this.api_key, this.endpointId, request)
+  }
+  async run(request: EndpointInputPayload): Promise<string> {
+    return run(this.api_key, this.endpointId, request)
+  }
+  async getStatus(requestId: string): Promise<EndpointOutput> {
+    return getStatus(this.api_key, this.endpointId, requestId)
+  }
+  async cancel(requestId: string) {
+    return cancel(this.api_key, this.endpointId, requestId)
+  }
+  async getHealth() {
+    return getHealth(this.api_key, this.endpointId)
+  }
+}
+
+const runpodSdk = (api_key: string) => ({
+  endpoint: (endpointId) => new Endpoint(api_key, endpointId),
+  //template...
+  //pod...
+})
+
+//usage
+const { RUNPOD_API_KEY } = process.env
+if (!RUNPOD_API_KEY) {
+  process.exit()
+}
+//OOP
+const runpod = runpodSdk(RUNPOD_API_KEY)
+const endpoint = runpod.endpoint("qhj9cwwco1pszj")
+const result = await endpoint.runsync({
+  input: {
+    prompt: "a photo of a horse the size of a Boeing 787",
+  },
+})
+//functional
+// const getResult = runRequest(RUNPOD_API_KEY, "qhj9cwwco1pszj")
+// const result = await getResult({
+//   input: {
+//     prompt: "a photo of a horse the size of a Boeing 787",
+//   },
+// })
