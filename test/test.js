21a289e88d8ceaa40d34145bfb9641aa6d10991b
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2023-10-23 22:34:13 -0400

    test functional way

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-import runpodSdk from "../dist/index.js"
+import runpodSdk, { runsync } from "../dist/index.js"
 
 const { RUNPOD_API_KEY, ENDPOINT_ID } = process.env
 if (!(RUNPOD_API_KEY && ENDPOINT_ID)) {
   process.exit()
 }
+//OOP
 const runpod = runpodSdk(RUNPOD_API_KEY)
 const endpoint = runpod.endpoint(ENDPOINT_ID)
 const result = await endpoint.runsync({
@@ -11,5 +12,14 @@ const result = await endpoint.runsync({
     prompt: "a photo of a horse the size of a Boeing 787",
   },
 })
-
 console.log(result)
+
+//functional
+const run = runsync(RUNPOD_API_KEY, ENDPOINT_ID)
+const res = await run({
+  input: {
+    prompt: "a photo of a horse the size of a Boeing 787",
+  },
+})
+
+console.log(res)
